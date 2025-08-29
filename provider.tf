ea757600c12b12228ffd5f06b875898216a557ed
Avnesh <avneshkushwaha@gmail.com>
2025-08-29 22:28:28 +0530

    add

--- provider.tf
+++ provider.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     azurerm = {
       source = "hashicorp/azurerm"
-      version = "4.32.0"
+      version = "4.42.0"
     }
   }
 }
@@ -16,9 +16,7 @@ terraform {
   }
 }
 
-
-
 provider "azurerm" {
   features {}
-  subscription_id = "76095b6e-0585-402e-8e43-281835aebfd1"
-}
+  subscription_id ="76095b6e-0585-402e-8e43-281835aebfd1"
+  }
